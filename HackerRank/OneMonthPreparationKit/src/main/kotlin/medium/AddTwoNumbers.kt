65ae1153a21e8b4d8d39d8ad8e89445a5fa30255
luis-jaramillo-dev <luisjaramillo.dev@gmail.com>
2022-10-05 18:56:05 -0500

    WIP: AddTwoNumbers challenge

---
+++ HackerRank/OneMonthPreparationKit/src/main/kotlin/medium/AddTwoNumbers.kt
@@ -0,0 +1,13 @@
+package medium
+
+class ListNode(var `val`: Int) {
+     var next: ListNode? = null
+}
+
+class AddTwoNumbers {
+
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+
+        return null
+    }
+}
